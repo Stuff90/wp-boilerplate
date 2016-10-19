677c3ede2f5a86936cd4eec2bd3ca46f6cbe9f74
Simon BERNARD <Stuff90@users.noreply.github.com>
2016-10-19 11:42:15 +0200

    [global] Allow page template identification by ID

--- wp-content/themes/faena/single.php
+++ wp-content/themes/faena/single.php
@@ -1,8 +1,17 @@
 <?php
-    $singleTemplate = locate_template('dist/singles/'. $post->post_type .'.php');
-    if (file_exists($singleTemplate)) {
-        require_once($singleTemplate);
-    } else {
-        header('Location: /404');
+    $pageTemplate = locate_template('dist/singles/single-'. $post->ID .'.php');
+    if (file_exists($pageTemplate)) {
+        if (have_posts()) : while (have_posts()) : the_post();
+            require_once($pageTemplate);
+        endwhile; endif;
+        die();
     }
+    $pageTemplate = locate_template('dist/singles/'. $post->post_type .'.php');
+    if (file_exists($pageTemplate)) {
+        if (have_posts()) : while (have_posts()) : the_post();
+            require_once($pageTemplate);
+        endwhile; endif;
+        die();
+    }
+    header('Location: /404.php');
 ?>
